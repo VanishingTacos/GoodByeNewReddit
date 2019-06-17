87c7947a1233ab9a69aed6d166e6475bdef72048
VanishingTacos <devonstuper@gmail.com>
2019-06-16 23:26:24 -0400

    Add files via upload

--- GoodByeNewReddit.js
+++ GoodByeNewReddit.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name           GoodByeNewReddit
-// @description    Auto Redirect to old.reddit.com from new.reddit.com
-// @version        1.0
-// @author         VanishingTacos
-// @namespace      https://www.reddit.com/user/VanishingTacos/
-// @homepageURL    https://www.reddit.com/user/VanishingTacos/
-// @updateURL    	 https://raw.githubusercontent.com/VanishingTacos/GoodByeNewReddit/master/GoodByeNewReddit.js
-// @downloadURL	   https://raw.githubusercontent.com/VanishingTacos/GoodByeNewReddit/master/GoodByeNewReddit.js
-// @grant          none
-// @run-at         document-start
-// @include        https://new.reddit.com/*
+// @name        GoodByeNewReddit
+// @version     1.0
+// @description Auto Redirect to old.reddit.com from new.reddit.com
+// @license     MIT
+// @author      VanishingTacos
+// @namespace   https://www.reddit.com/user/VanishingTacos/
+// @include     https://new.reddit.com/*
+// @run-at      document-start
+// @grant       none
+// @updateURL   https://raw.githubusercontent.com/VanishingTacos/GoodByeNewReddit/master/GoodByeNewReddit.js
+// @downloadURL https://raw.githubusercontent.com/VanishingTacos/GoodByeNewReddit/master/GoodByeNewReddit.js
 // ==/UserScript==
 
 		window.location.replace("https://old.reddit.com" + window.location.pathname);
